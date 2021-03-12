952bb63edfa997ce80026a583107c967b8e21739
Heber Gustavo <heber.gbarbosa@hotmail.com>
2021-03-12 16:55:17 -0300

    Criação da pagina de Item de Sorteio

--- Sorteio/Controllers/HomeController.cs
+++ Sorteio/Controllers/HomeController.cs
@@ -22,16 +22,11 @@ namespace Sorteio.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        
+        [Route("Sorteio")]
+        public IActionResult Sorteio()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
