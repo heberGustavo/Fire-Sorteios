286ba6201b44128588e9be0d332a66f32664bd74
Heber Gustavo <heber.gbarbosa@hotmail.com>
2021-04-12 15:46:57 -0300

    Criação dos controllers de sorteio

---
+++ Sorteio/Controllers/SorteiosController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Sorteio.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sorteio.Controllers
+{
+    public class SorteiosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Novo()
+        {
+            return View();
+        }
+    }
+}
