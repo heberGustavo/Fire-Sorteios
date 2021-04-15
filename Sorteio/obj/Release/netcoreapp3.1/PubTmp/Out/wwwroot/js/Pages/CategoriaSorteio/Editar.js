1def604a6afba8bdf9b7d321180c0839172cc6c2
Heber Gustavo <heber.gbarbosa@hotmail.com>
2021-04-15 11:04:47 -0300

    .

---
+++ Sorteio/obj/Release/netcoreapp3.1/PubTmp/Out/wwwroot/js/Pages/CategoriaSorteio/Editar.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+
+});
+
+function Editar(elemento, id) {
+
+    var nomeCategoriaAtual = $(elemento).parent().parent().find('td:nth-child(1)').text();
+
+    $('#idCategoriaSorteioSelecionado').val(id);
+    $('#nomeCategoria').val(nomeCategoriaAtual);
+
+    $('#modalNovaCategoria').modal();
+}
---
+++ Sorteio/obj/Release/netcoreapp3.1/PubTmp/Out/wwwroot/js/Pages/Login/Index.js
@@ -0,0 +1,50 @@
+﻿$(document).ready(function () {
+
+});
+
+function RealizarLogin() {
+
+    if (VerificaCamposPreenchidos()) {
+
+        var jsonBody = {
+            "email": $('#email').val(),
+            "senha": $('#senha').val()
+        };
+
+        $.ajax({
+            url: "/Login/Login",
+            type: "POST",
+            contentType: 'application/json; charset=UTF-8',
+            dataType: "json",
+            data: JSON.stringify(jsonBody),
+            success: function (response) {
+                console.log(response);
+                if (response.erro) {
+                    alert(response.mensagem);
+                }
+                else {
+                    alert('tudo certo')
+
+                }
+            },
+            error: function (response) {
+                alert(response);
+            }
+        });
+    }
+    
+}
+
+function VerificaCamposPreenchidos() {
+
+    if (IsNullOrEmpty($('#email').val().trim())) {
+        MostrarModalErroCampoObrigatorioNaoPreenchido('Email');
+        return false;
+    }
+    else if (IsNullOrEmpty($('#senha').val().trim())) {
+        MostrarModalErroCampoObrigatorioNaoPreenchido('Senha');
+        return false;
+    }
+
+    return true;
+}
